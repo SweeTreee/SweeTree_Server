25fc12039363b689080e2cca450e0fc4a2d82191
JeongHeumChoi <andyc707@gmail.com>
2024-01-02 03:36:28 +0900

    feat: User model 생성

--- sweetree_server/accounts/models.py
+++ sweetree_server/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractUser, PermissionsMixin):
+    box_type = models.CharField(max_length=10, default='None')
+    social_auth = models.CharField(max_length=2, default='None')
+    is_box_public = models.BooleanField(default=False)
+
+    class UserSocialAuth(models.TextChoices):
+        NONE = 'NN', 'None'
+        KAKAO = 'KK', 'Kakao'
+        GOOGLE = 'GG', 'Google'
+        NAVER = 'NV', 'Naver'
