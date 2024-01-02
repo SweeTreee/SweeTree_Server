c0d3b04c0269c893f05d1ffae72e09894de3edbd
JeongHeumChoi <andyc707@gmail.com>
2024-01-02 18:02:20 +0900

    feat: User model field 변수 추가

--- sweetree_server/accounts/models.py
+++ sweetree_server/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
 
 class User(AbstractUser, PermissionsMixin):
+    box_name = models.CharField(max_length=20, null=False) # need modify
     box_type = models.CharField(max_length=10, default='None')
     social_auth = models.CharField(max_length=2, default='None')
     is_box_public = models.BooleanField(default=False)
