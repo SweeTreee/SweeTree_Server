a3f9e5876e5ef6d68052b015c637d181358316e7
JeongHeumChoi <andyc707@gmail.com>
2024-01-09 17:24:15 +0900

    feat: 유저 모델 생성

--- sweetree_server/accounts/models.py
+++ sweetree_server/accounts/models.py
@@ -1,15 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
 
-class User(AbstractUser, PermissionsMixin):
-    box_name = models.CharField(max_length=20, null=False) # need modify
-    box_type = models.CharField(max_length=10, default='None')
-    social_auth = models.CharField(max_length=2, default='None')
-    is_box_public = models.BooleanField(default=False)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    class UserSocialAuth(models.TextChoices):
-        NONE = 'NN', 'None'
-        KAKAO = 'KK', 'Kakao'
-        GOOGLE = 'GG', 'Google'
-        NAVER = 'NV', 'Naver'
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # box_name = models.CharField(max_length=20, null=False) # need modify
+    # box_type = models.CharField(max_length=10, default='None')
+    # is_box_public = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
