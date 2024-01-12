973102e6fe62e7143fbe9adfd5978d163e5f81c2
JeongHeumChoi <andyc707@gmail.com>
2024-01-12 16:48:53 +0900

    fix: Letter 모델 field 수정

--- sweetree_server/letter/models.py
+++ sweetree_server/letter/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Letter(models.Model):
-    title       = models.CharField(max_length=255, verbose_name="Title")
-    content     = models.TextField(verbose_name="Content")
-    x           = models.FloatField(verbose_name="Longitude")
-    y           = models.FloatField(verbose_name="Latitude")
+    box_id      = models.ForeignKey('box.Box', on_delete=models.CASCADE, verbose_name="Box id")
+    choco_type  = models.CharField(max_length=4, verbose_name="Choco type")
+    nickname    = models.CharField(max_length=20, verbose_name="Nickname")
+    contents    = models.TextField(verbose_name="Contents")
     created_at  = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     ip_address  = models.GenericIPAddressField(null=True, editable=False)
 
