9e19c06d0741c7cd3d287a1bb1055d5500eaf006
JeongHeumChoi <andyc707@gmail.com>
2024-01-12 16:25:30 +0900

    feat: Box 모델 생성

--- sweetree_server/box/models.py
+++ sweetree_server/box/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Box(models.Model):
+    user_id         = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    box_name        = models.CharField(max_length=20, null=False)
+    box_type        = models.CharField(max_length=10, default='None')
+    is_box_public   = models.BooleanField(default=False)
+    created_at      = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'box'
