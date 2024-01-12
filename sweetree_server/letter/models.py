c92859f2142c91da3db2b4d2fd58d544426643dc
JeongHeumChoi <andyc707@gmail.com>
2024-01-12 16:27:03 +0900

    feat: letter 모델 생성

--- sweetree_server/letter/models.py
+++ sweetree_server/letter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Letter(models.Model):
+    title       = models.CharField(max_length=255, verbose_name="Title")
+    content     = models.TextField(verbose_name="Content")
+    x           = models.FloatField(verbose_name="Longitude")
+    y           = models.FloatField(verbose_name="Latitude")
+    created_at  = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    ip_address  = models.GenericIPAddressField(null=True, editable=False)
+
+    class Meta:
+        db_table = 'letter'
