19450c3293bb47ad5c12004d80265b985e1c5bf0
JeongHeumChoi <andyc707@gmail.com>
2024-01-14 03:17:34 +0900

    feat: Box CRU 구현

--- sweetree_server/box/urls.py
+++ sweetree_server/box/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
 from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    BoxViewSet
+)
 
+
+'''
+    /box/                               # 생성      POST
+    /box/<int:pk>                       # 단일 조회  GET
+    /box/<int:pk>/                      # 수정      PATCH
+'''
+
+
+router = DefaultRouter()
+router.register('box', BoxViewSet)
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
