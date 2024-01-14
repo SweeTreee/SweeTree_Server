3367362e035aea3e3e72f37f0974897fecbec8c0
JeongHeumChoi <andyc707@gmail.com>
2024-01-14 14:17:26 +0900

    fix: url 경로 수정

--- sweetree_server/accounts/urls.py
+++ sweetree_server/accounts/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include
 
 
 urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('dj_rest_auth.urls')),
+    path('', include('dj_rest_auth.registration.urls')),
 ]
