6c4d4b2f560db1f24f9172d32718dc3de482a997
JeongHeumChoi <andyc707@gmail.com>
2024-01-20 23:05:15 +0900

    feat: 구글 소셜로그인 구현

--- sweetree_server/accounts/urls.py
+++ sweetree_server/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.conf.urls import include
+from accounts.views import GoogleLoginView
 
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('dj_rest_auth.registration.urls')),
+
+    # path('', include('allauth.urls')),
+    path('google/login', GoogleLoginView.as_view(), name='api_accounts_google_oauth'),
 ]
