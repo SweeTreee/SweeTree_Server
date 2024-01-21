4018f7bb8cbcb9ff85de724f512318f30124f2bf
JeongHeumChoi <andyc707@gmail.com>
2024-01-21 16:46:03 +0900

    feat: 카카오 소셜 로그인 구현

--- sweetree_server/accounts/urls.py
+++ sweetree_server/accounts/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.conf.urls import include
-from accounts.views import GoogleLoginView
-
+from accounts.views import (
+    GoogleLoginView,
+    KakaoLoginView,
+)
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('dj_rest_auth.registration.urls')),
 
-    # path('', include('allauth.urls')),
+    path('', include('allauth.urls')),
+    path('kakao/login', KakaoLoginView.as_view(), name='api_accounts_kakao_oauth'),
     path('google/login', GoogleLoginView.as_view(), name='api_accounts_google_oauth'),
 ]
